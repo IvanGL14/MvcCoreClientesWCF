aba21b080cfe90d4304c0f14eb8d4505b6c5a434
IvanGL14 <vanchi127@gmail.com>
2022-02-16 14:24:01 +0100

    commit

---
+++ MvcCoreClientesWCF/Controllers/CountriesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreClientesWCF.Services;
+using ServicioCountries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreClientesWCF.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private ServicesCountries services;
+
+        public CountriesController(ServicesCountries services)
+        {
+            this.services = services;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            tCountryCodeAndName[] countries = await this.services.GetCountries();
+            return View(countries);
+        }
+
+        public async Task<IActionResult> Detalles(string iso)
+        {
+            FullCountryInfoResponse country = await this.services.InformacionPais(iso);
+            return View(country);
+        }
+    }
+}
---
+++ MvcCoreClientesWCF/Services/ServicesCountries.cs
@@ -0,0 +1,34 @@
+﻿using ServicioCountries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreClientesWCF.Services
+{
+    public class ServicesCountries
+    {
+        //EL NOMBRE DEL OBJETO SIEMPRE SERA UN CLIENT
+        //QUE SE LLAMARÁ COMO EL NOMBRE DEL SERVICIO QUE HEMOS
+        //VISTO EN LA ANTERIOR PAMTALLA
+        CountryInfoServiceSoapTypeClient client;
+
+        public ServicesCountries()
+        {
+            this.client = new CountryInfoServiceSoapTypeClient(CountryInfoServiceSoapTypeClient.EndpointConfiguration.CountryInfoServiceSoap);
+        }
+
+        public async Task<tCountryCodeAndName[]> GetCountries()
+        {
+            ListOfCountryNamesByNameResponse response = await this.client.ListOfCountryNamesByNameAsync();
+            tCountryCodeAndName[] objetos = response.Body.ListOfCountryNamesByNameResult;
+            return objetos;
+        }
+
+        public async Task<FullCountryInfoResponse> InformacionPais(string isoCountry)
+        {
+            FullCountryInfoResponse country = await this.client.FullCountryInfoAsync(isoCountry);           
+            return country;
+        }
+    }
+}
